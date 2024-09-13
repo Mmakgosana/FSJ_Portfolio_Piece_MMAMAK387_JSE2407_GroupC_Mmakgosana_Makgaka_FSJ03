8ca6cb02c243afe6801af3f57bffdd698326108f
Mmakgosana <makgakammakgosana@gmail.com>
2024-09-13 08:53:42 +0200

    Added a span element to display the current page number in the Pagination component.

--- app/page.js
+++ app/page.js
@@ -78,6 +78,7 @@ function Pagination({ currentPage }) {
           </button>
         </Link>
       )}
+      <span className="text-lg font-bold text-gray-700">Page {pageNum}</span>
       <Link href={`/?page=${nextPage}`}>
         <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300">
           Next Page
