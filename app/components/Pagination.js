0e23d69352af5aed49348d4976216e6b45e38ee1
Mmakgosana <makgakammakgosana@gmail.com>
2024-09-13 13:23:38 +0200

    Update button hover colors in ProductPage and Pagination components

--- app/components/Pagination.js
+++ app/components/Pagination.js
@@ -17,7 +17,7 @@ export default function Pagination({ currentPage }) {
       )}
       <span className="text-lg font-bold text-gray-700">Page {pageNum}</span>
       <Link href={`/?page=${nextPage}`}>
-        <button className="px-4 py-2 bg-pink-400 text-white rounded-lg hover:bg-blue-700 transition-colors duration-300">
+        <button className="px-4 py-2 bg-pink-400 text-white rounded-lg hover:bg-purple-500 transition-colors duration-300">
           Next Page
         </button>
       </Link>
