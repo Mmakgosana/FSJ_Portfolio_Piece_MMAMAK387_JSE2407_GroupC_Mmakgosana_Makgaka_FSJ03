9b9f7346238ddf709e0f34389c7160055c18c67f
Mmakgosana <makgakammakgosana@gmail.com>
2024-10-10 09:04:22 +0200

    Refactored ProductDetails component, removed Firestore operations, and updated Firebase config

--- app/firebaseconfig.js
+++ app/firebaseconfig.js
@@ -3,12 +3,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDFKl_nAx2hYW17CeWvKtk85Gvi26xiE2I",
-    authDomain: "next-ecommerce-d37e4.firebaseapp.com",
-    projectId: "next-ecommerce-d37e4",
-    storageBucket: "next-ecommerce-d37e4.appspot.com",
-    messagingSenderId: "104970121917",
-    appId: "1:104970121917:web:2a642ec9f1ae35d87e3dd4"
+    apiKey: "AIzaSyCTw1Wq0Q_E3eOvOELbPr7Q8Ht9jc4oizc",
+    authDomain: "next-ecommerce-e3d6c.firebaseapp.com",
+    projectId: "next-ecommerce-e3d6c",
+    storageBucket: "next-ecommerce-e3d6c.appspot.com",
+    messagingSenderId: "398258298281",
+    appId: "1:398258298281:web:64cee2b828590d5eab78b8"
   };
 
 // Initialize Firebase
